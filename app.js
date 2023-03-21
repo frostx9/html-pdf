01583f5f931281acf265617accf50cc1d7d46f5e
Saumya Som <saumyasom92@gmail.com>
2023-03-21 23:26:22 +0530

    Html-pdf

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
 const fs = require('fs')
 const path = require('path')
-const pdf = require("html-pdf")
+const puppeteer = require("puppeteer") // New Method with Puppeteer
+
 
 module.exports = {
   async htmpPdf(req, res) {
     try {
-      const options = {
-        height: "11.25in",
-        width: "8.5in",
-        header: {
-          height: "20mm"
-        },
-        footer: {
-          height: "20mm",
-        },
-      }
-
-      const data = await ejs.renderFile(path.join(__dirname, "../../views/", "report.ejs"), // ejs file location and file name
-        {
-          links: modifiedBackLinks, // Data taht pass to ejs file
+      const compile = async function () {
+        const data = await ejs.renderFile(path.join(__dirname, "../../views/", "report.ejs"), {
+          month: moment().format("MMMM"), links: modifiedBackLinks, keyWords: modifiedKeyWord, blogs: blogArr, socialMedias: modifiedsocialMediaReport, daReport: report.daReport, daDate: moment(report.daDate).format("MMM DD, YYYY"), paReport: report.paReport, paDate: moment(report.paDate).format("MMM DD, YYYY"), ssReport: report.ssReport, ssDate: moment(report.ssDate).format("MMM DD, YYYY"), notes: report.notes
         }, { async: true })
 
-      pdf.create(data, options).toFile("report.pdf", async (err, data) => {
-        if (err) {
-          console.log(err)
-          res.send(err)
-        } else {
-          console.log(data)
-          res.send("File created successfully")
-          const dataLocation = await aws.uploadFile("report.pdf") // aws s3 file upload
-          console.log(dataLocation) // return s3 bucket location
-          const monthName = moment().format("MMMM")
-          const project = await Project.findOne({ _id: id }).select("id clientEmail name")
-            .populate("_client", "name")
+        return data
+      };
 
-          if (project.clientEmail !== undefined) { // for mailing the s3 file data location
+      (async function () {
+        try {
+          const browser = await puppeteer.launch({
+            executablePath: '/usr/bin/chromium-browser' // Download chromium manually if not work - sudo apt-get install chromium-browser
+          });
+
+          const page = await browser.newPage();
+          const content = await compile();
+          await page.setContent(content)
+
+          await page.pdf({
+            path: 'report.pdf',
+            format: "A4",
+            printBackground: false
+          })
+
+          const dataLocation = await aws.uploadFile("report.pdf")
+          const monthName = moment().format("MMMM")
+
+          if (project.clientEmail !== undefined) {
             mail("client-sendreport", {
               to: project.clientEmail,
               subject: `Project Report for ${monthName}`,
@@ -44,11 +43,22 @@ module.exports = {
                 url: dataLocation,
                 project: project.name,
                 month: monthName
-              }
+              },
+              attachments: mailAttachment
             })
           }
+          const path = "./report.pdf"
+          fs.unlinkSync(path)
+          console.log("File removed:", path)
+
+          await browser.close()
+          return res.status(200).json({ error: false, message: "Report Sucessfully Send" })
+
+        } catch (error) {
+          console.log("error", error);
         }
-      })
+      })()
+
     } catch (error) {
       console.log(error)
     }
